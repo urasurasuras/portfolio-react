8327e7803769b4eb6fea466a7901efb9790752fa
uras <uras.oran.htc@gmail.com>
2023-04-21 22:49:36 -0400

    Added routing Added projects page Added logic for navbar if on homepage or not

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,61 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Burgermenu from "./Burgermenu";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 
 function NavbarComponent() {
-  const navRef = useRef();
-  const showNavbar = (event) => {
-    console.log(event.target.value);
-    navRef.current.classList.toggle("collapse");
-  };
+  const location = useLocation();
+  const locationPathname = location.pathname;
+  const [headerBgOpaque, setHeaderBgOpaque] = useState(false);
+  const [atHome, setAtHome] = useState();
+
+  useEffect(() => {
+    // execute on location change
+    console.log("Location changed!", locationPathname);
+    if (locationPathname !== "/") {
+      setAtHome(false);
+      console.log("Location not home", locationPathname);
+      setHeaderBgOpaque(true);
+      window.removeEventListener("scroll", changeColor);
+    } else {
+      // At home
+      setAtHome(true);
+      console.log("Location home", locationPathname);
+      setHeaderBgOpaque(false);
+      window.addEventListener("scroll", changeColor);
+    }
+  }, [location]);
 
   // Change navbar color while scrolling
-  const [headerBgColor, setHeaderBgColor] = useState('rgba(0 0 0 / 1)');
-  const [colorAlpha, setColorAlpha] =useState(0);
-  const changeColor = ()=>{
-    // setColorAlpha((window.scrollY - 90) /200);
-    // console.log(colorAlpha);
-    // setHeaderBgColor('rgba(0 0 0 / '+ colorAlpha+')');
-    if (window.scrollY >= 90){
-      setHeaderBgColor(true);
-    }else {
-      setHeaderBgColor(false);
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setHeaderBgOpaque(true);
+    } else {
+      setHeaderBgOpaque(false);
     }
-  }
+  };
 
-  window.addEventListener('scroll', changeColor);
   return (
-    <Navbar style={{backgroundColor: headerBgColor}} 
-    className={headerBgColor ? 'header-bg-scrolling' :'header-bg'} 
-    variant="dark" expand="lg" scrolling="dark" fixed="top">
-      <Container >
-        <Navbar.Brand href="#home">Uras Oran</Navbar.Brand>
+    <Navbar
+      style={{ backgroundColor: headerBgOpaque }}
+      className={headerBgOpaque ? "header-bg-scrolling" : "header-bg"}
+      variant="dark"
+      expand="lg"
+      scrolling="dark"
+      fixed="top"
+    >
+      <Container>
+        <Navbar.Brand href={atHome ? "#home" : "/"}>Uras Oran</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" align="end" >
-          <Nav className="ml-auto" >
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav" align="end">
+          <Nav className="ml-auto">
+            <Nav.Link href="/#about">About</Nav.Link>
+            <Nav.Link href="/#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/#contact">Contact</Nav.Link>
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
---
+++ src/pages/Projects.js
@@ -0,0 +1,220 @@
+function Projects() {
+  return (
+    <div className="jumbotron container">
+      <h1 className="display-4">Project index</h1>
+      <p className="lead">
+        This page contains all the projects done for this class, the{" "}
+        <i className="fab fa-github"></i> icon will take you to the repo and the
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          className="bi bi-box-arrow-right"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+          />
+          <path
+            fillRule="evenodd"
+            d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+          />
+        </svg>
+        icon will take you to the web page
+      </p>
+      <hr className="my-4" />
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Hydrametry Software</td>
+            <td>
+              Homepage for Hydrametry Software, we're making a game, check it
+              out!
+            </td>
+            <td>
+              <a href="https://hydrametry.com/">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-box-arrow-right"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                  />
+                </svg>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>IS 117</td>
+            <td>
+              This repository is the first one to contain the first few
+              assignments for this class. It contains assignments 1-3 in
+              individual folders
+            </td>
+            <td>
+              <a href="https://github.com/urasurasuras/is117">
+                <i className="fab fa-github"></i>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>How the Tnternet Works</td>
+            <td>
+              This webpage goes through how the Internet works. It also has a
+              "GitHub Pages" page
+            </td>
+            <td>
+              <a href="https://github.com/urasurasuras/HowTheInternetWorks">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://urasurasuras.github.io/HowTheInternetWorks/">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-box-arrow-right"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                  />
+                </svg>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>History of the Internet</td>
+            <td>History of the Internet. Team project (empty content)</td>
+            <td>
+              <a href="https://github.com/urasurasuras/IS117---History-of-the-Internet">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://urasurasuras.github.io/IS117---History-of-the-Internet/">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-box-arrow-right"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                  />
+                </svg>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Cloud Technology </td>
+            <td>
+              Informational webpage about Docker, Kubernetes, containerization
+              and cloud technology. Team project (empty content)
+            </td>
+            <td>
+              <a href="https://github.com/urasurasuras/is117-dockerpage">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://urasurasuras.github.io/is117-dockerpage//">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-box-arrow-right"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                  />
+                </svg>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">6</th>
+            <td>The Internet </td>
+            <td>
+              Combination of "How the Internet Works" and "History of the
+              Internet" web pages
+            </td>
+            <td>
+              <a href="https://github.com/urasurasuras/is117-the-internet">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://urasurasuras.github.io/is117-the-internet/">
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-box-arrow-right"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                  />
+                </svg>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <hr className="my-4" />
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Projects;
