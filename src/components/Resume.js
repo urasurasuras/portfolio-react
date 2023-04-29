f142c07b9e83c6b83c4eae46bdae40d1042f1362
uras <uras.oran.htc@gmail.com>
2023-04-29 14:39:25 -0400

    Added projects link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,13 +4,16 @@ function Resume() {
   return (
     <section className="page-section bg-dark text-white">
       <div className="container text-center">
-        <h2 className="mb-4">Take a look at my resume!</h2>
+        <h2 className="mb-4">Take a look at my resume, and my projects!</h2>
         <a
-          className="btn btn-light btn-xl"
+          className="btn btn-light btn-xl mr-3"
           href={Endpoints.Resume}
           target="_blank"
         >
           View PDF
+        </a>
+        <a className="btn btn-light btn-xl mr-3" href={Endpoints.Projects}>
+          Projects
         </a>
       </div>
     </section>
